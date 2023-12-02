c3e3c376d343a02d6e60eaa900e2e92b8661a46b
Lerin17 <lerinowoade17@gmail.com>
2023-12-02 12:40:19 +0100

    minor ui update #1

--- learn-app/components/liveClasses/ClassChat.tsx
+++ learn-app/components/liveClasses/ClassChat.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ClassChat = () => {
   return (
-    <div className='text-white' >
-      <div className='' >
+    <div className='text-white ' >
+      <div className='text-black' >
         ClassChat
       </div>
      
